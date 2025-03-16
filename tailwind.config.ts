107dc5355fff7ddd2432fe8645c68975ef79da3a
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2025-03-16 19:11:14 +0100

    add tailwind comfig file

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'tailwindcss';
+
+// tailwind.config.ts
+
+const config: Config = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#2491DF",
+                secondary: "#EBF7FF",
+                text_clr: "#001E33",
+                clr_lightgrey: "#F9FAFB",
+                clr_grey: "#98A2B3",
+                clr_red: "#E26E6A",
+                clr_yellow: "#F3A218",
+                clr_lightyellow: "#FEF6E7",
+                clr_green: "#00CD8E",
+                clr_lightgreen: "#EBFFF9",
+
+            },
+        }
+    },
+    plugins: [],
+};
+
+export default config;
