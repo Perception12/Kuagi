42077721e349e347613b97c170c52c6a7b2753fe
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2025-04-28 22:54:10 +0100

    update business hum

--- components/CurvedCarousel.tsx
+++ components/CurvedCarousel.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Image from "next/image";
 import Image1 from "@/assets/img_business1.png";
 import Image2 from "@/assets/img_business2.png";
@@ -8,22 +9,12 @@ const images = [Image1, Image2, Image3];
 
 export default function CurvedCarousel() {
   return (
-    <div className="relative justify-center items-center hidden lg:flex">
-      <div className="relative flex justify-center perspective">
-        <div className="flex curved-container w-full">
-          {images.map((src, index) => (
-            <div key={index} className="curved-item w-full">
-              <Image
-                src={src}
-                alt={`Image ${index + 1}`}
-                width={300}
-                height={200}
-                className="object-cover"
-              />
-            </div>
-          ))}
-        </div>
-      </div>
+    <div className="flex gap-6">
+
+      {images.map((image, index) => (
+        <Image src={image} key={index} alt="slider" className="object-cover" />
+      ))}
+
     </div>
   );
 }
